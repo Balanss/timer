f1e44ef45e7c286b25546caa5037141ff20a8cd8
gilmon purperhart <gilmonpurry@hotmail.com>
2022-04-28 17:09:09 -0300

    deployed website

--- src/comp/Product.jsx
+++ src/comp/Product.jsx
@@ -6,9 +6,8 @@ return(
 
   <div className="hold-text">
 <h2> {props.time}  </h2>
-<p className="yellow-warning">  {props.month} / {props.day} </p>
+<p className="yellow-warning">  {props.month} / {props.day} /{props.year} </p>
 <p className="warning"> {props.dates} Days left for THT!! </p>
-<p> {props.year} </p>
  </div>
 )
 

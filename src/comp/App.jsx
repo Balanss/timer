515a120c59ec120f042463c42df170f7603a12f1
gilmon purperhart <gilmonpurry@hotmail.com>
2022-04-28 20:33:15 -0300

    deployed website

--- src/comp/App.jsx
+++ src/comp/App.jsx
@@ -4,34 +4,34 @@ import Product from "./Product";
 
 
 
+
 function createDate(date){
   let time = new Date()
   const time1 = time.getMonth()+1
   let t = new Date()
   const time2 = t.getDate()
+  let y = new Date()
+  const time3 = y.getMonth()+2
 
 
-
-
-
-  if(date.month === time1){
+  if(date.month  === time1){
     return(
 
 <div className="all-items">
  <Product
    time={date.name}
    month={date.month}
+   year={date.year}
    day={date.day}
    dates={date.dates - time2}
-
    />
-
-
 </div>
+
+
 
     )
   }
-  else {
+  else  {
     return null;
   }
 

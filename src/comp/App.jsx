39a9ab9e800d84c12b4cc2b501df1b18bd018a3d
gilmon purperhart <gilmonpurry@hotmail.com>
2022-04-29 14:19:32 -0300

    updated

--- src/comp/App.jsx
+++ src/comp/App.jsx
@@ -11,31 +11,32 @@ function createDate(date){
   let t = new Date()
   const time2 = t.getDate()
   let y = new Date()
-  const time3 = y.getMonth()+2
+  const time3 = y.getFullYear()
+  let doubleMonth = new Date()
+  const dm = doubleMonth.getMonth()+2
 
 
-  if(date.month  === time1){
+
+  if((date.month == time1 && (date.year === time3))   ){
     return(
+      <div className="all-items">
+       <Product
+         time={date.name}
+         month={date.month}
+         year={date.year}
+         day={date.day}
+         dates={date.dates - time2}
+         />
 
-<div className="all-items">
- <Product
-   time={date.name}
-   month={date.month}
-   year={date.year}
-   day={date.day}
-   dates={date.dates - time2}
-   />
-</div>
 
+      </div>
 
 
     )
   }
   else  {
     return null;
-  }
-
-
+  };
 
 
 
--- src/comp/Product.jsx
+++ src/comp/Product.jsx
@@ -6,7 +6,7 @@ return(
 
   <div className="hold-text">
 <h2> {props.time}  </h2>
-<p className="yellow-warning">  {props.month} / {props.day} /{props.year} </p>
+<p className="yellow-warning">  {props.month} / {props.day} /{props.year} {props.stats} </p>
 <p className="warning"> {props.dates} Days left for THT!! </p>
  </div>
 )
